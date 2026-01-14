c8c9880405f4ad44d2a17e527e6ec3c170486dde
Jaydeep <jaysgaikwad123@gmail.com>
2026-01-14 16:22:22 +0530

    Added Splash Screen of the app

--- app/src/main/java/com/jaydeep/fruithub/MainActivity.kt
+++ app/src/main/java/com/jaydeep/fruithub/MainActivity.kt
@@ -19,29 +19,9 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             FruitHubTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                SplashScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    FruitHubTheme {
-        Greeting("Android")
-    }
-}
---
+++ app/src/main/java/com/jaydeep/fruithub/SplashScreen.kt
@@ -0,0 +1,33 @@
+package com.jaydeep.fruithub
+
+import android.window.SplashScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+@Preview (showSystemUi = true)
+fun SplashScreen(modifier: Modifier = Modifier) {
+    Column(modifier = modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally) {
+
+        Image(
+            painter = painterResource(R.drawable.fruit_hub)
+            , contentDescription = "Fruit Hub Logo",
+
+            modifier=modifier.size(120.dp)
+        )
+        // Add your splash screen UI components here
+    }
+}
